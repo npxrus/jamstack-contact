f9b045829fb2ee1dcc24d3f2c5ad15ff093e14ca
npxrus <npxrus@yandex.com>
2021-01-28 16:16:08 +0300

    feat: add loading spinner

--- src/components/form.js
+++ src/components/form.js
@@ -7,6 +7,7 @@ const INITIAL_STATE = {
   email: "",
   subject: "",
   body: "",
+  status: "IDLE",
 };
 
 const reducer = (state, action) => {
@@ -14,6 +15,10 @@ const reducer = (state, action) => {
     case "updateFieldValue":
       return { ...state, [action.field]: action.value };
 
+    case "updateStatus":
+      return { ...state, status: action.status };
+
+    case "reset":
     default:
       return INITIAL_STATE;
   }
@@ -21,6 +26,8 @@ const reducer = (state, action) => {
 
 const Form = () => {
   const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
+
+  const setStatus = (status) => dispatch({ type: "updateStatus", status });
 
   const updateFieldValue = (field) => (event) => {
     dispatch({
@@ -32,53 +39,79 @@ const Form = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    setStatus("PENDING");
 
     // TODO: send the message
   };
 
+  if (state.status === "SUCCESS") {
+    return (
+      <p className={styles.success}>
+        Message sent!
+        <button
+          type="reset"
+          onClick={() => dispatch({ type: "reset" })}
+          className={`${styles.button} ${styles.centered}`}
+        >
+          Reset
+        </button>
+      </p>
+    );
+  }
+
   return (
-    <form className={styles.form} onSubmit={handleSubmit}>
-      <label className={styles.label}>
-        Name
-        <input
-          className={styles.input}
-          type="text"
-          name="name"
-          value={state.name}
-          onChange={updateFieldValue("name")}
-        />
-      </label>
-      <label className={styles.label}>
-        Email
-        <input
-          className={styles.input}
-          type="email"
-          name="email"
-          value={state.name}
-          onChange={updateFieldValue("email")}
-        />
-      </label>
-      <label className={styles.label}>
-        Subject
-        <input
-          className={styles.input}
-          type="text"
-          name="subject"
-          value={state.name}
-          onChange={updateFieldValue("subject")}
-        />
-      </label>
-      <label className={styles.label}>
-        Body
-        <textarea
-          className={styles.input}
-          name="body"
-          value={state.name}
-          onChange={updateFieldValue("body")}
-        />
-      </label>
-      <button className={styles.button}>Send</button>
-    </form>
+    <>
+      {state.status === "ERROR" && (
+        <p className={styles.error}>Something went wrong. Please try again.</p>
+      )}
+      <form
+        className={`${styles.form} ${
+          state.status === "PENDING" && styles.pending
+        }`}
+        onSubmit={handleSubmit}
+      >
+        <label className={styles.label}>
+          Name
+          <input
+            className={styles.input}
+            type="text"
+            name="name"
+            value={state.name}
+            onChange={updateFieldValue("name")}
+          />
+        </label>
+        <label className={styles.label}>
+          Email
+          <input
+            className={styles.input}
+            type="email"
+            name="email"
+            value={state.email}
+            onChange={updateFieldValue("email")}
+          />
+        </label>
+        <label className={styles.label}>
+          Subject
+          <input
+            className={styles.input}
+            type="text"
+            name="subject"
+            value={state.subject}
+            onChange={updateFieldValue("subject")}
+          />
+        </label>
+        <label className={styles.label}>
+          Body
+          <textarea
+            className={styles.input}
+            name="body"
+            value={state.body}
+            onChange={updateFieldValue("body")}
+          />
+        </label>
+        <button className={styles.button}>Send</button>
+      </form>
+    </>
   );
 };
 
